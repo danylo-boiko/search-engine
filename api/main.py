3f6c32a2f4e225470190f248b7e5a6b83b30e0e9
Danylo Boiko <danielboyko02@gmail.com>
2024-03-05 23:44:50 +0200

    Minor refactoring

--- api/main.py
+++ api/main.py
@@ -5,9 +5,9 @@ from api.settings import settings
 from api.v1.api import api_router
 
 
-app = FastAPI(title=settings.PROJECT_TITLE)
+app = FastAPI(title=settings.API_PROJECT_TITLE)
 
 if settings.CORS_ORIGINS:
     app.add_middleware(CORSMiddleware, allow_origins=settings.CORS_ORIGINS, allow_methods=["GET"])
 
-app.include_router(api_router, prefix=settings.V1_PREFIX)
+app.include_router(api_router, prefix=settings.API_V1_PREFIX)
