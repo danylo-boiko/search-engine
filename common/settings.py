7a2ab502be83b758a5bc9f2ba989f549bc5ad400
Danylo Boiko <danielboyko02@gmail.com>
2024-03-16 19:30:45 +0200

    Add summarizer

--- common/settings.py
+++ common/settings.py
@@ -6,7 +6,8 @@ from common.enums import Language
 class Settings(BaseSettings):
     DEFAULT_LANGUAGE: Language = Language.ENGLISH
     SUPPORTED_LANGUAGES: set[Language] = {Language.ENGLISH, Language.UKRAINIAN}
-    EMBEDDING_MODEL_NAME: str = "paraphrase-multilingual-mpnet-base-v2"
+    EMBEDDING_MODEL_NAME: str = "sentence-transformers/paraphrase-multilingual-mpnet-base-v2"
+    SUMMARIZER_MODEL_NAME: str = "csebuetnlp/mT5_multilingual_XLSum"
     MONGO_SEARCH_ENGINE_DB: str = "search-engine"
     MONGO_CONNECTION_STRING: str
 
--- indexer/indexer.py
+++ indexer/indexer.py
@@ -2,9 +2,11 @@ import logging
 
 from time import time
 
+from numpy import ndarray
 from pymongo import MongoClient
 from pymongo.errors import PyMongoError, DuplicateKeyError
 from sentence_transformers import SentenceTransformer
+from transformers import pipeline
 
 from common import settings
 from indexer.models import CrawledPage, PageSummary
@@ -26,6 +28,7 @@ class Indexer:
             self.content_items_collections[language] = self.database.get_collection(f"{language}_content_items")
 
         self.embedding_model = SentenceTransformer(settings.EMBEDDING_MODEL_NAME)
+        self.summarizer = pipeline("summarization", model=settings.SUMMARIZER_MODEL_NAME)
 
     def __del__(self) -> None:
         self.client.close()
@@ -44,10 +47,29 @@ class Indexer:
 
             self.content_items_collections[language].insert_many([{
                 "page_id": inserted_page.inserted_id,
-                "content": content_item
+                "content": content_item,
+                "content_embedding": self.__get_text_embedding(content_item).tolist()
             } for content_item in page.content_items])
 
             logging.info(f"Inserted page {inserted_page.inserted_id} {page.title}")
         except PyMongoError as error:
             if not isinstance(error, DuplicateKeyError):
                 logging.error(error)
+
+    def __get_text_embedding(self, text: str) -> ndarray:
+        tokens_count = self.__get_tokens_count(text)
+
+        if tokens_count > self.embedding_model.max_seq_length:
+            text = self.summarizer(
+                text,
+                max_length=self.embedding_model.max_seq_length,
+                min_length=int(self.embedding_model.max_seq_length * 0.8),
+                do_sample=False
+            )[0]["summary_text"]
+
+            logging.info(f"Tokens count reduced from {tokens_count} to {self.__get_tokens_count(text)}")
+
+        return self.embedding_model.encode(text)
+
+    def __get_tokens_count(self, text: str) -> int:
+        return len(self.embedding_model.tokenizer.tokenize(text))
